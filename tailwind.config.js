0258b83a588b76774d7337ed8d6841435ea5b932
Oluwatomisin Taiwo <oluwatomisinoluwafemi@gmail.com>
2021-05-12 03:08:29 +0100

    base setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,25 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          mid: '#FBFBFB',
+          deep: '#F8F8F8',
+        },
+        blue: {
+          top: '#3CC7F2',
+          mid: '#2BB7E7',
+          bottom: '#139ED4',
+        }
+      },
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/ui"),
+    require('@tailwindcss/forms'),
+  ],
 }
